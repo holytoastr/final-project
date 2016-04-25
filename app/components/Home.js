3d3ab210a4d190661aff87a86836e6d3f844a4db
Melissa Miller <lis.d.miller@gmail.com>
2016-04-25 08:54:18 -0400

    Sets up routes and achieves hello world

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <div>Hello world!</div>
+    )
+  }
+});
+
+module.exports = Home;
---
+++ app/index.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+var routes = require('./config/routes');
+
+ReactDom.render(routes, document.getElementById('app'));
